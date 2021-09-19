f6fe7f94433320308ebf30ca90df62e9072aad82
Veelkoov <veelkoov@protonmail.com>
2021-09-19 20:41:20 +0200

    Updated DB dump

--- artisans_values.sql
+++ artisans_values.sql
@@ -7,4 +7,6 @@ INSERT INTO artisans_values VALUES(913,484,'IS_MINOR','False');
 INSERT INTO artisans_values VALUES(914,484,'WORKS_WITH_MINORS','False');
 INSERT INTO artisans_values VALUES(915,185,'IS_MINOR','False');
 INSERT INTO artisans_values VALUES(916,185,'WORKS_WITH_MINORS','False');
+INSERT INTO artisans_values VALUES(917,323,'IS_MINOR','False');
+INSERT INTO artisans_values VALUES(918,323,'WORKS_WITH_MINORS','True');
 COMMIT;
